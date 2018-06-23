03bd177290edeb23222c250447da6bbacdd189fc
rebecahss <31423619+rebecahss@users.noreply.github.com>
2018-06-23 13:01:22 -0300

    Add files via upload

--- serv.py
+++ serv.py
@@ -1,9 +1,10 @@
 #Servidor
 
 import socket
+import os
 
 IP = ""
-Port = 30801
+Port = 30802
 mensagem = [b'Recebido pelo servidor']
 
 
@@ -22,18 +23,40 @@ try:
 
 	while True:
 		msg = obj.recv(1024)
-		
-		print ("O que foi enviado:", msg)
 		msg1 = msg.decode('utf-8')
+		
+		print ("O que foi enviado:", msg1)
+		
 		if msg1 == 'ls':
 			print("Você escolheu o comando ls")
 			obj.send(b'Resposta do servidor')
 
-		elif msg1== 'sair':
+		elif msg1 == 'enviar':
+			print("recebendo arquivo")
+			obj.send(b'Voce esta enviando arquivo')
+
+			flname = 'recebido.txt'
+			fyle =open(flname,'wb')
+			fyle.write(obj.recv(6053))
+			fyle.close()
+			print('Arquivo recebido')
+
+		elif msg1 == 'receb':
+			obj.send(b'Voce esta recebendo arquivo.')
+
+			flname = 'serv_to_cliente.txt'
+			fyle = open(flname, 'rb')
+			arquivo = fyle.read(6053)
+			obj.send(arquivo)
+			print('Aquivo enviado!')
+
+
+
+		elif msg1 == 'sair':
 			break
 
 		else:
-			print("Comando Inválido")
+			print('Comando Inválido')
 	
 				
 
